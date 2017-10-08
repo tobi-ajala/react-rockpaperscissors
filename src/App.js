b7fc3f8e63e6777900a03249a35fdd417745d559
Tobi Ajala <tobi-ajala@hotmail.com>
2017-10-08 23:53:38 +0100

    complete commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-import Results from './Results'
 import './index.css'
+import Results from './Results'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {games:[]}
+  constructor(props) { //constructor component called before it's mounted
+    super(props) //forks the props and sets the state with the initial props
+    this.state = {games:[]} //start an empty game state
     this.handleMove = this.handleMove.bind(this)
   }
 
   handleMove(playerMove) {
-    var cpuMove = this.getCPUMove(),
-        result = this.scoreGame(playerMove, cpuMove)
+    var cpuMove = this.getCPUMove(), //get the current state of the cpuMove function
+        result = this.scoreGame(playerMove, cpuMove) //get the current result from the playerMove and cpuMove functions
 
+    console.log('Player chose: ' + playerMove + '. CPU chose: ' + cpuMove + '.') //log user & cpu options in console
+    console.log('The winner is ' + (result === 1 ? 'player.' : result === 2 ? 'CPU.' : 'draw.')) //log the result in the console
 
-    console.log('Player chose: ' + playerMove + '. CPU chose: ' + cpuMove + '.')
-    console.log('The winner is ' + (result === 1 ? 'player.' : result === 2 ? 'CPU.' : 'draw.'))
+    this.setState((prev,props) => ({ //function to set the new game state from user & cpu choices actioned above
+      games: [...prev.games, { player: playerMove, cpu: cpuMove, winner: result }] //game state takes previous states and increments with new game result state
+    }))
+  }
+
+  handleCMoves(playerMove) { //computer same handleMove funtion in the console except for computer vs computer
+    var cpuMove = this.getCPUMove(), //get the current state of the cpuMove function
+        result = this.scoreGame(playerMove, cpuMove) //get the current result from the playerMove and cpuMove functions
+
+    console.log('Computer 1 chose: ' + playerMove + '. Computer 2 chose: ' + cpuMove + '.')
+    console.log('The winner is ' + (result === 1 ? 'Computer 1.' : result === 2 ? 'Computer 2.' : 'draw.'))
 
     this.setState((prev,props) => ({
       games: [...prev.games, { player: playerMove, cpu: cpuMove, winner: result }]
@@ -25,40 +36,46 @@ class App extends Component {
   }
 
   getCPUMove() {
-    var moves = ['R', 'P', 'S'],
-        move = Math.floor(Math.random() * 3)
-    return moves[move]
+    var moves = ['R', 'P', 'S'], //declaring rock, paper, scissors as R, P, S variables
+        move = Math.floor(Math.random() * 3) //establishes a random whole number to represent the cpu choice
+    return moves[move] //return the cpu random whole number as a R, P, S value
   }
 
-  scoreGame (p1Move, p2Move) {
-    if (p1Move === p2Move) { return 'draw' }
-
-    if ( (p1Move === 'R' & p2Move === 'S') ||
-         (p1Move === 'P' & p2Move === 'R') ||
-         (p1Move === 'S' & p2Move === 'P') ) {
-      return 1
+  scoreGame (p1Move, cpuMove) {
+    if (p1Move === cpuMove) { return 'draw' } //if user & cpu moves = the same return a draw state
+    if ( (p1Move === 'R' & cpuMove === 'S') || //if conditions for occurance of R, P, S user choice
+         (p1Move === 'P' & cpuMove === 'R') ||
+         (p1Move === 'S' & cpuMove === 'P') ) {
+      return 1 //return console 'winner is..' statement
     } else {
       return 2
     }
   }
 
-  render() {
+  restartGame(event) { //restart game function
+    this.setState({ games: [] }); //empty the games state array, hence resetting the game state
+  }
+
+  render() { //rendering the app interface
     return (
       <div className="App">
         <div className="gameCanvas">
           <h1>Choose your weapon</h1>
           <div className="actions">
+            {/*defining R, P, S images as buttons that call on the handleMove function when clicked*/}
             <button onClick={()=>{this.handleMove('R')}}><img src={ require('./images/rock.svg') } alt="rock" /></button>
             <button onClick={()=>{this.handleMove('P')}}><img src={ require('./images/paper.svg') } alt="paper" /></button>
             <button onClick={()=>{this.handleMove('S')}}><img src={ require('./images/scissors.svg') } alt="scissors" /></button>
-            <Results games={this.state.games}></Results>
+            <Results games={this.state.games}></Results> {/*defining the state i.e. score of the current games*/}
             <br /><br />
+            <button type="button" onClick={ this.restartGame.bind(this) }><span>Restart Game</span></button>   {/*calling the restartGame function using bind to create a new bound function*/}
+            <br /><br />
+            <button type="button" onClick={ () => this.handleCMoves(this.getCPUMove()) }><span>Click to simulate Computer vs Computer</span></button>
           </div>
-
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App; //part of es6 module system
--- src/Results.js
+++ src/Results.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Results extends Component {
+
   constructor(props) {
     super(props)
     this.playerWins = this.playerWins.bind(this)
     this.playerLosses = this.playerLosses.bind(this)
-    this.playerWinPct = this.playerWinPct.bind(this)
+    this.playerDraws = this.playerDraws.bind(this)
   }
 
-  playerWins() {
+  playerWins() { //function - if the game state is the player wins, add 1 to wins status
     return this.props.games.reduce( (sum, g) => { return g.winner === 1 ? sum+1 : sum }, 0 )
   }
-
-  playerLosses() {
+  playerLosses() { //function - if the game state is the player loses, add 1 to loses status
     return this.props.games.reduce( (sum, g) => { return g.winner === 2 ? sum+1 : sum }, 0 )
   }
-
-  playerWinPct() {
-    if (this.props.games.length === 0) return '-'
-    return ((this.playerWins() / this.props.games.length).toFixed(2) * 100) + '%'
+  playerDraws() { //function - if the game state is a draw, add 1 to draw status
+    return this.props.games.length-this.playerWins()-this.playerLosses();
   }
 
   render() {
     return (
       <div className="results">
         <ul>
-          <li>Wins: {this.playerWins()}</li>
+          <li>Wins: {this.playerWins()}</li> {/*rendering the results interface*/}
           <li>Losses: {this.playerLosses()}</li>
+          <li>Draws: {this.playerDraws()}</li>
           <br />
           <li>Games: {this.props.games.length}</li>
         </ul>
